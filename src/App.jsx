95d235d57e847e8b60cd249d625ce6562680d1be
Mobeen-Dev <mubeenqamar29@gmail.com>
2025-03-11 20:05:29 +0500

    env variables setup

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          {import.meta.env.VITE_APPWRITE_URL}
         </p>
       </div>
       <p className="read-the-docs">
